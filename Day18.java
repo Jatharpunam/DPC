9ab3b6034369511f1c624c6f2d2ce182e4755278
Jatharpunam <punamjathar19@gmail.com>
2025-09-04 10:31:14 +0530

    Create Day18.java
    Count the Number of Divisors of a Number.

---
+++ Day18.java
@@ -0,0 +1,27 @@
+public class DivisorCount {
+
+    public static int countDivisors(int n) {
+        int count = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i * i == n) {
+                    count++;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countDivisors(12));   
+        System.out.println(countDivisors(18));   
+        System.out.println(countDivisors(29));   
+        System.out.println(countDivisors(100)); 
+        System.out.println(countDivisors(1));    
+        System.out.println(countDivisors(997));  
+        System.out.println(countDivisors(36));   
+    }
+}
